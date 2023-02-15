914410cb8290103259de3d3728e67a2e97df93c0
sgeinitz <69814800+sgeinitz@users.noreply.github.com>
2023-02-15 16:57:29 -0700

    Update assign2tests.py

--- assign2tests.py
+++ assign2tests.py
@@ -64,6 +64,7 @@ class TestAssign2Functions(unittest.TestCase):
         #self.assertGreater(self.shuf_mergeRes3[1]*1.5, self.shuf_mergeRes2[1])
         self.assertGreater(self.shuf_mergeRes3[1]*2.5, self.shuf_mergeRes2[1])
         self.assertGreater(self.rev_quickResA[1]/10, self.rev_quickResB[1])
-        self.assertGreater(self.m3 * 2, self.m2)
+        #self.assertGreater(self.m3 * 2, self.m2)
+        self.assertGreater(self.m3 * 2.5, self.m2)
         self.assertGreater(self.m2 * 2, self.m3)
 
